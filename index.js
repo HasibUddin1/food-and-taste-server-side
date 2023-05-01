20dba02238b5b3b77bf7df8bee59b1eec99497f0
IconicLuck <uhasib959@gmail.com>
2023-05-02 03:09:31 +0600

    uploaded data to the server

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
+const chefData = require('./data/data.json')
+
 const port = process.env.PORT || 5000
 
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello from the server')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefData)
 })
 
 app.listen(port, () => {
